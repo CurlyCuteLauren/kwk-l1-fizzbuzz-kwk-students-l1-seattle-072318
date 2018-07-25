1395042c34e3d9828851370db2d2f745afce5b77
CurlyCuteLauren <lnziegele@gmail.com>
2018-07-25 16:09:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -12,29 +12,3 @@ end
 puts fizzbuzz(int= 3)
 
 
-def fizzbuzz(int= 5)
-  if (int % 3 == 0) && (int % 5 == 0)
-     "Fizzbuzz"
-  elsif (int % 3 == 0)
-     "Fizz"
-  elsif (int % 5 == 0)
-      "Buzz"
-else 
-   "NIL"
-  end
-end
-puts fizzbuzz(int= 5)
-
-
-def fizzbuzz(int= 15)
-  if (int % 3 == 0) && (int % 5 == 0)
-     "Fizzbuzz"
-  elsif (int % 3 == 0)
-     "Fizz"
-  elsif (int % 5 == 0)
-      "Buzz"
-else 
-   "NIL"
-  end
-end
-puts fizzbuzz(int= 15)
