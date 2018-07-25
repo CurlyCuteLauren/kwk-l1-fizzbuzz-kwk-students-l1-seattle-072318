237b68848f614eb7548de13605fd944991648f9e
CurlyCuteLauren <lnziegele@gmail.com>
2018-07-25 16:06:46 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(int= 3)
+  if (int % 3 == 0) && (int % 5 == 0)
+     "Fizzbuzz"
+  elsif (int % 3 == 0)
+     "Fizz"
+  elsif (int % 5 == 0)
+      "Buzz"
+else 
+   "NIL"
+  end
+end
+puts fizzbuzz(int= 3)
+
+
